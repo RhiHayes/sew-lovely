a23b72f25673dfe79d94ca88cc1a42b5761adf88
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2023-05-13 16:26:43 -0500

    Added some things for gh pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <>
    <Navbar />
     <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={Home} basename={process.env.PUBLIC_URL}/>
         <Route exact path="/products" component={Products}/>
         <Route exact path="/product/:sku" component={ProductList}/>
         <Route exact path="/cart" component={Cart} />
